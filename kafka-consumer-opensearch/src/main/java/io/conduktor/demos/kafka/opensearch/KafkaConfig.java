1897cda47ff76702ae7b58be24fde65f4207addd
Oleh Pysarenko <oa.pisarenko@gmail.com>
2023-07-01 10:26:28 +0100

    moved kafka consumer configuration to properties

--- kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/KafkaConfig.java
+++ kafka-consumer-opensearch/src/main/java/io/conduktor/demos/kafka/opensearch/KafkaConfig.java
@@ -1,28 +1,19 @@
 package io.conduktor.demos.kafka.opensearch;
 
-import java.util.Properties;
-import org.apache.kafka.clients.CommonClientConfigs;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
-import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class KafkaConfig {
 
+  private final KafkaProperties kafkaProperties;
+
   @Bean
-  public KafkaConsumer<String, String> createKafkaConsumer() {
-    String groupId = "consumer-opensearch-demo";
-
-    Properties properties = new Properties();
-    properties.setProperty(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-    properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-    properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-    properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-    properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
-
-    return new KafkaConsumer<>(properties);
+  public KafkaConsumer<String, String> kafkaConsumer() {
+    return new KafkaConsumer<>(kafkaProperties.buildConsumerProperties());
   }
 }
